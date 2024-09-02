e02ba27f3c4dbb47c58556144e39ed8538eb93ea
itslu666 <philipp.j6y@gmail.com>
2024-09-02 22:03:10 +0200

    added window movement

--- modules/tkinter_manager.py
+++ modules/tkinter_manager.py
@@ -19,11 +19,21 @@ def make_window(wid, hgt, img):
     panel.pack(side="top", fill="both", expand="yes")
     panel.image = img_tk
 
-    panel.bind("<B1-Motion>", lambda e: move(root, e))
+    root.bind("<ButtonPress-1>", lambda e: get_start(e, root))
+    root.bind("<B1-Motion>", lambda e: move(root, e, root.start_x, root.start_y))
 
     root.bind("<q>", lambda e: root.destroy())
     root.mainloop()
 
-def move(root, e):
-    x, y = root.winfo_pointerxy()
+def move(root, e, start_x, start_y):
+    x = root.winfo_pointerx() - start_x
+    y = root.winfo_pointery() - start_y
     root.geometry(f"+{x}+{y}")
+
+def get_start(e, root):
+    start_x = e.x_root - root.winfo_x()
+    start_y = e.y_root - root.winfo_y()
+
+    root.start_x = start_x
+    root.start_y = start_y
+
