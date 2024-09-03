45627971b9ad927fcc9631b298ad910ad37db8a9
itslu666 <philipp.j6y@gmail.com>
2024-09-03 10:51:29 +0200

    added process handlin + switched from label to canvas

--- modules/tkinter_manager.py
+++ modules/tkinter_manager.py
@@ -2,35 +2,43 @@ import tkinter as tk
 from PIL import ImageTk
 
 def make_window(wid, hgt, img):
+    # make window
     root = tk.Tk()
     root.wm_attributes('-type', 'splash')
     root.attributes('-topmost', True)
 
+    # get screen hight and width
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
+    # calc geometry if image and make window same size
     x = screen_width - wid - 30
     y = 30
     root.geometry(f"{wid}x{hgt}+{x}+{y}")
 
+    # make image
     img_tk = ImageTk.PhotoImage(img)
 
-    panel = tk.Label(root, image=img_tk)
-    panel.pack(side="top", fill="both", expand="yes")
-    panel.image = img_tk
+    canvas = tk.Canvas(root, width=wid, height=hgt)
+    canvas.pack()
+    canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
 
+    # binds
     root.bind("<ButtonPress-1>", lambda e: get_start(e, root))
     root.bind("<B1-Motion>", lambda e: move(root, e, root.start_x, root.start_y))
-
     root.bind("<q>", lambda e: root.destroy())
+
+    # show window
     root.mainloop()
 
 def move(root, e, start_x, start_y):
+    # move window with pointer in same position
     x = root.winfo_pointerx() - start_x
     y = root.winfo_pointery() - start_y
     root.geometry(f"+{x}+{y}")
 
 def get_start(e, root):
+    # get and set pointer position
     start_x = e.x_root - root.winfo_x()
     start_y = e.y_root - root.winfo_y()
 
---
+++ pinboard.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+import os
+
+def run_script():
+    # Bestimme den absoluten Pfad zur main.py
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    main_script_path = os.path.join(script_dir, 'main.py')
+
+    # Bestimme den Python-Befehl basierend auf der Python-Version
+    python_command = sys.executable  # Dies gibt den Pfad zur aktuell verwendeten Python-Version zurück
+
+    # Starte main.py im Hintergrund
+    subprocess.Popen([python_command, main_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if __name__ == "__main__":
+    run_script()
